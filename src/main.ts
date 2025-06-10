11c70d8eded6133f77396ea3bf68779f09bd2277
NoobForever-MG <vito.lezama@materiagris.pe>
2025-06-09 21:04:13 -0500

    [FIX]Cors IP Front changed

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ 
-        origin: ['http://192.168.31.193:9636'], 
+        origin: ['http://192.168.31.190:9636'], 
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
         credentials: true, 
     });
